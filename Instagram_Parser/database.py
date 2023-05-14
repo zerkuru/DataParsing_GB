fcfdefdd1b5dd87a6f5d2a9a71f3cbe9394f34d7
Zinaida Erkuru <zikonnikova@gmail,com>
2023-05-15 00:29:39 +0200

    HW8

--- Instagram_Parser/database.py
+++ Instagram_Parser/database.py
@@ -4,40 +4,38 @@ import json
 
 
 def makebase():
-    connector_db = sqlite3.connect('news.db')
+    connector_db = sqlite3.connect('inst.db')
     cur = connector_db.cursor()
 
     #tables making
     
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS news(
-      newsid INT PRIMARY KEY,
-      title TEXT,
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+      userid INT PRIMARY KEY,
+      name TEXT,
       href TEXT,
-      source TEXT,
-      datestring TEXT);
+      instid TEXT);
+      """)
+    connector_db.commit()
+    cur.execute("""CREATE TABLE IF NOT EXISTS subscribers(
+      connectionid INT PRIMARY KEY,
+      userid INT FOREIGN KEY users.userid,
+      subscriberid INT FOREIGN KEY users.userid);
       """)
     connector_db.commit()
     connector_db.close()
 
 
-def inputnews_json(json_string):
+def inputnew_json(json_string):
     connector_db = sqlite3.connect('db.sqlite')
     cursor = connector_db.cursor()
     
-    cursor.execute("""CREATE TABLE IF NOT EXISTS news(
-      newsid INT PRIMARY KEY,
-      title TEXT,
-      href TEXT,
-      source TEXT,
-      datestring TEXT);
-      """)
-    
-    columns = ['title','href','source', 'datestring']
+    columns = ['name','href','instid']
     for row in json_string:
         keys= tuple(row[c] for c in columns)
-        cursor.execute('insert into news values(?,?,?)',keys)
-        print(f'{row["title"]} data inserted Succefully')
+        cursor.execute('insert into users values(?,?,?)',keys)
+        print(f'{row["name"]} data inserted Succefully')
+        
 
     connector_db.commit()
     connector_db.close()
@@ -47,24 +45,23 @@ def finduser (username):
     connector_db = sqlite3.connect('db.sqlite')
     cursor = connector_db.cursor()
     
-    cursor.execute("""CREATE TABLE IF NOT EXISTS news(
-      newsid INT PRIMARY KEY,
-      title TEXT,
-      href TEXT,
-      source TEXT,
-      datestring TEXT);
+    userid = cursor.execute("""SELECT * FROM users where name = name);
       """)
     
-    columns = ['title','href','source', 'datestring']
-    for row in json_string:
-        keys= tuple(row[c] for c in columns)
-        cursor.execute('insert into news values(?,?,?)',keys)
-        print(f'{row["title"]} data inserted Succefully')
-
-    connector_db.commit()
     connector_db.close()
+    return userid 
 
 def getsubscribers(userid):
+    connector_db = sqlite3.connect('db.sqlite')
+    cursor = connector_db.cursor()
+    
+    sublist = cursor.execute("""SELECT name FROM users where userid in (SELECT subscriberid FROM subscribers where userid = userid);
+      """)
+    
+    connector_db.close()
+    return sublist 
+
+
         connector_db = sqlite3.connect('db.sqlite')
     cursor = connector_db.cursor()
     
@@ -89,14 +86,11 @@ def getfollowed(userid):
     connector_db = sqlite3.connect('db.sqlite')
     cursor = connector_db.cursor()
     
-    columns = ['title','href','source', 'datestring']
-    for row in json_string:
-        keys= tuple(row[c] for c in columns)
-        cursor.execute('insert into news values(?,?,?)',keys)
-        print(f'{row["title"]} data inserted Succefully')
-
-    connector_db.commit()
+    sublist = cursor.execute("""SELECT name FROM users where userid in (SELECT userid FROM subscribers where subscriberid = userid);
+      """)
+    
     connector_db.close()
+    return sublist 
      
 
  
